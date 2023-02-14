e4f0023807d780584a6aac80c719511bf7ba6e7f
Cristian B <ccbonillar@uqvirtual.edu.co>
2023-02-14 13:09:59 -0500

    cambios cards de productos

--- src/app/views/orders/orders.component.ts
+++ src/app/views/orders/orders.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalDismissReasons, NgbDatepickerModule,NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-orders',
@@ -8,9 +8,10 @@ import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstra
 })
 export class OrdersComponent implements OnInit {
 
-  images = [1055, 194, 368].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  images = [102,91,93,193,110].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
   open() {
+    //this.modalService.open(content, { size: 'xl' });
 		const modalRef = this.modalService.open(OrdersComponent);
 		modalRef.componentInstance.name = 'World';
 	}
