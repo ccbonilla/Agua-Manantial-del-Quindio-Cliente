059f13351b438b310c41733b75b9ce205f399bd7
Cristian B <ccbonillar@uqvirtual.edu.co>
2023-05-08 10:19:28 -0500

    cambios order

--- src/app/views/orders/orders.component.ts
+++ src/app/views/orders/orders.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalDismissReasons, NgbDatepickerModule,NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
+
 import { ProductService } from 'src/app/services/products/product.service';
 import { Product } from '../../models/product';
 
@@ -13,8 +14,14 @@ export class OrdersComponent implements OnInit {
 
   products: Product[] = [];
 
-  images = [102,91,93,193,110].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  parte1: Product[] = [];
+  parte2: Product[] = [];
+  parte3: Product[] = [];
+  
+  productSize: number = 0;
+  currentItem = 0;
 
+  images = [102,91,93,193,110].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
   constructor(
     private productService: ProductService
@@ -22,11 +29,23 @@ export class OrdersComponent implements OnInit {
 
 
   ngOnInit(): void {
+    console.log("El componente se ha inicializado");
+    setInterval(() => {
+      this.currentItem = (this.currentItem + 1) % 3;
+    }, 5000);
+    this.getProducts();
   }
 
   getProducts() {
     this.productService.get('list').subscribe((prods) => {
     this.products = prods;
+    console.log("Result productos: "+prods);
+    this.productSize = Object.keys(prods).length;
+    console.log("Result size: "+this.productSize);
+
+    this.parte1 = prods.slice(0, Math.ceil(prods.length / 3));
+    this.parte2 = prods.slice(Object.keys(this.parte1).length, Math.ceil(prods.length / 3) * 2);
+    this.parte3 = prods.slice(Object.keys(this.parte1).length + Object.keys(this.parte2).length);
     });
   }
 }
