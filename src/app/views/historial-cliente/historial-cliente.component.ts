b548e7a1bfe10f2f3a2949a69d60ec3f576ac8ce
Cristian B <ccbonillar@uqvirtual.edu.co>
2023-05-16 10:12:14 -0500

    cambios clientes historial pedidos

--- src/app/views/historial-cliente/historial-cliente.component.ts
+++ src/app/views/historial-cliente/historial-cliente.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UserService } from '../../services/users/users.service';
 
 @Component({
   selector: 'app-historial-cliente',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistorialClienteComponent implements OnInit {
 
-  constructor() { }
+  customers: User[] = [];
+
+  cliente: User = new User();
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.getUsers();
   }
 
+  getUsers() {
+    this.userService.get('find-by-id/1').subscribe((users) => {
+      this.customers = users;
+      //this.cliente = users;
+
+      
+      
+      //console.log('SERVICIO GET USER 1: '+this.customers);
+      //console.log('SERVICIO GET USER 2: '+Object.keys(users).length);
+      //console.log('SERVICIO GET USER 3: '+JSON.stringify(users));
+    });
+  }
 }
