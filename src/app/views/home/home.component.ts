b37846d70a0789ce08a1e63e062d8235a2aabcdd
Cristian B <ccbonillar@uqvirtual.edu.co>
2023-06-08 17:48:34 -0500

    home imagenes

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -7,11 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  backgroundImage1 = 'url(https://www.65ymas.com/uploads/s1/15/22/65/tipos-de-agua-embotellada1.jpeg)';
+  backgroundImage1 = 'url(https://cdn.pixabay.com/photo/2013/12/17/20/10/bubbles-230014_1280.jpg)';
 
   backgroundImage2 = 'url(https://3.bp.blogspot.com/-40TK5LMSn0w/WsTrAXGu0yI/AAAAAAAADBY/YuG1NKgCpCEu0G09bUQSNq2KsPLA_0LZgCLcBGAs/s1600/manantial.jpg)';
 
-  backgroundImage3 = 'url(https://www.65ymas.com/uploads/s1/15/22/65/tipos-de-agua-embotellada1.jpeg)';
+  backgroundImage3 = 'url(https://images.unsplash.com/photo-1561041695-d2fadf9f318c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80)';
+
+  backgroundImage4 = 'url(https://cdn.pixabay.com/photo/2017/02/02/15/15/bottle-2032980_1280.jpg)';
 
   images = [
     {
