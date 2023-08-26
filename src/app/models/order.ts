954b75061fda7c1439a9095a6e248ee4c2e3c7f5
Cristian B <ccbonillar@uqvirtual.edu.co>
2023-08-26 16:48:40 -0500

    importacion service Order cambios variables para order class

--- src/app/models/order.ts
+++ src/app/models/order.ts
@@ -1,26 +1,9 @@
-import { User } from './user';
+import { Product } from './product';
 export class Order {
-  order_id: number = 0;
   user_id: number = 0;
   order_date: string = '';
-  order_state: number = 0;
-  value: number = 0;
   discount: number = 0;
   payment_type_id: number = 0;
-  product_cant: number = 0;
-  product_id: number = 0;
-  customer: User = {
-    user_id: 0,
-    name: '',
-    lastname: '',
-    email: '',
-    phone: '',
-    address: '',
-    user_type_id: 0,
-    previous_user_type_id: 0,
-    count: 0,
-    user_type_name: '',
-    password:  '',
-    identification: 0,
-  };
+  
+  products: Product[] = [];
 }
